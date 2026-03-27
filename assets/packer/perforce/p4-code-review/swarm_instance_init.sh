63de683c9ef0af2678981841e729a466a6ec35f9
Justin Boswell <jboswell@secretdimension.com>
2026-03-27 14:53:56 -0700

    Print the security level

--- assets/packer/perforce/p4-code-review/swarm_instance_init.sh
+++ assets/packer/perforce/p4-code-review/swarm_instance_init.sh
@@ -175,7 +175,7 @@ if [ -f "$SWARM_CONFIG" ]; then
 
   P4_SECURITY=$(P4USER=$P4D_SUPER P4PASSWD=$P4D_SUPER_PASSWD P4PORT=$P4D_PORT /opt/perforce/bin/p4 configure show security | cut -f2 -d= | cut -f1 -d" ")
   if [[ -n "$P4_SECURITY" && $(($P4_SECURITY)) -ge 3 ]]; then
-    log_message "P4 Server requires tickets, generating ticket..."
+    log_message "P4 Server requires tickets (security level $P4_SECURITY), generating ticket..."
     P4_TICKET=$(P4USER=$P4D_SUPER P4PASSWD=$P4D_SUPER_PASSWD P4PORT=$P4D_PORT /opt/perforce/bin/p4 login -a -p)
     php -r "
       \$config = include '$SWARM_CONFIG';
