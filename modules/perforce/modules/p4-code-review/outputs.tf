0541985b1034900688c11c5bd6b9c66cb6463f1b
Andrew Ettensohn <andrew@secretdimension.com>
2026-03-27 12:57:14 -0700

    Remove execution role output for p4cr

--- modules/perforce/modules/p4-code-review/outputs.tf
+++ modules/perforce/modules/p4-code-review/outputs.tf
@@ -42,8 +42,3 @@ output "ebs_volume_id" {
   value       = aws_ebs_volume.swarm_data.id
   description = "The ID of the EBS volume storing P4 Code Review persistent data"
 }
-
-output "execution_role_id" {
-  value       = aws_iam_role.task_execution_role.id
-  description = "The default role for the service task"
-}
