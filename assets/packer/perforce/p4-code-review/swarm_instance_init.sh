ea4e404273c05134a15c96d8a0693d32b5ae8a88
Justin Boswell <jboswell@secretdimension.com>
2026-03-27 15:06:24 -0700

    Manually specify port and passwd for p4 login

--- assets/packer/perforce/p4-code-review/swarm_instance_init.sh
+++ assets/packer/perforce/p4-code-review/swarm_instance_init.sh
@@ -179,7 +179,7 @@ if [ -f "$SWARM_CONFIG" ]; then
   P4_SECURITY=$(P4USER=$P4D_SUPER P4PASSWD=$P4D_SUPER_PASSWD P4PORT=$P4D_PORT /opt/perforce/bin/p4 configure show security | cut -f2 -d= | cut -f1 -d" ")
   if [[ -n "$P4_SECURITY" && $(($P4_SECURITY)) -ge 3 ]]; then
     log_message "P4 Server requires tickets (security level $P4_SECURITY), generating ticket..."
-    P4_TICKET=$(P4PASSWD=$P4D_SUPER_PASSWD P4PORT=$P4D_PORT /opt/perforce/bin/p4 login -a -p $P4D_SUPER)
+    P4_TICKET=$(/opt/perforce/bin/p4 -P $P4D_SUPER_PASSWD -p $P4D_PORT login -a -p $P4D_SUPER)
     php -r "
       \$config = include '$SWARM_CONFIG';
       if (!isset(\$config['p4'])) {
